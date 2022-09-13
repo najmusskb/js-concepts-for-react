48725700666097e4fc1a4acb40c41f03dd38a85a
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-09-13 15:41:46 +0600

    local storage

---
+++ 8.shortcut.js
@@ -0,0 +1,53 @@
+//1. -------------------truthy and folsy //eassy sikhe nite parbo 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//2.--------------------------------------------ternary operator
+
+
+const money = 120;
+let food;
+if (money > 100) {
+    food = 'biriyani'
+}
+else {
+    food = 'cha biskut';
+}
+
+// ternary operator
+let food1 = money > 100 ? 'biriyani' : 'chabiskut';
+console.log(food1)
+
+
+
+
+
+//------------------------- number to string shortcut
+const num = 12;
+const numstr = num + '';
+// console.log(numstr)
+
+
+
+
+// ----------------------------str to number convertion
+const number = '12';
+const numnum = +number;
+console.log(numnum);
+
---
+++ 9-storage.js
@@ -0,0 +1,15 @@
+const addToLocalstorage = () => {
+
+    const inputId = document.getElementById('storage-id');
+    const id = inputId.value;
+
+
+    const inputvalue = document.getElementById('storage-value')
+    const value = inputvalue.value;
+
+    //localstorage value set 
+    localStorage.setItem(id, value);
+
+    inputId.value = '';
+    inputvalue.value = '';
+}
